bdc49c70d46ec86358f6f6979ae5920b872a411f
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-02-18 10:28:03 +0100

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+gallery.addEventListener('click', onGaleryElementClick);
+
+function createGalleryItemsMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `<div class="gallery__item"><a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        /></a></div>`
+    }).join('');
+}
+
+
+function onGaleryElementClick(e) {
+    e.preventDefault();
+    if (!e.target.dataset.source) {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img
+    src="${e.target.dataset.source}" 
+    width="800" 
+    height="600"
+    />
+    `, {
+        onShow: () => document.addEventListener('keydown', onCloseModal),
+        onClose: () => document.removeEventListener('keydown', onCloseModal)
+    });
+
+    instance.show();
+
+    function onCloseModal(e) {
+        if (e.code === 'Escape') instance.close();
+    }
+}
+
+
+
+
+
+
