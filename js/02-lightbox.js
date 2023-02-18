82f9e4d78423453c2ea530f00956bef9d4647d83
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-02-18 11:10:44 +0100

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGalleryItemsMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a></li>`
+    }).join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
